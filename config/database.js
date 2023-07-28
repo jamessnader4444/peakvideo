9e5f4b95fb14fe6c8bfc6b03adec9149e6545501
jamessnader4444 <138589695+jamessnader4444@users.noreply.github.com>
2023-07-28 06:43:31 -0700

    connect to clearDB

--- config/database.js
+++ config/database.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("cms", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  "heroku_d8665f1f40c1bb2",
+  "b56562578eae2d",
+  "1404ba06",
+  {
+    host: "1404ba06@us-cdbr-east-06.cleardb.net",
+    dialect: "mysql",
+  }
+);
 
 export default db;
